ae853ef9e3317aa205fefcc8e5077a77b46eb864
thekadiroglu <116295346+thekadiroglu@users.noreply.github.com>
2023-10-12 16:09:28 +0300

    NextJs with Json

---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react'
+import AddToCard from './AddToCard';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <AddToCard/>
+    </div>
+  )
+}
+
+export default ProductCard
